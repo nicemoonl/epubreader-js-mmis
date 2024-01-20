c7dfb829ecc668da663ef4e4e4c7fe943cf846aa
vite <nuget@list.ru>
2024-01-20 12:51:49 +0300

    refactoring the Metadata class

--- src/toolbar/metadata.js
+++ src/toolbar/metadata.js
@@ -1,32 +1,29 @@
-import { UIPanel, UIText } from '../ui.js';
+import { UIPanel, UIText } from "../ui.js";
 
 export class MetadataPanel extends UIPanel {
 
 	constructor(reader) {
 
 		super();
-		super.setId('metadata');
+		super.setId("metadata");
 
-		this.title = new UIText().setId('book-title');
-		this.creator = new UIText().setId('book-creator');
-		this.separator = new UIText().setId('book-title-separator');
+		const title = new UIText().setId("book-title");
+		const creator = new UIText().setId("book-creator");
+		const separator = new UIText().setId("book-title-separator");
 
-		super.add([this.title, this.separator, this.creator]);
+		super.add([title, separator, creator]);
 
 		//-- events --//
 
-		reader.on('metadata', (meta) => {
+		reader.on("metadata", (meta) => {
 
-			this.init(meta);
+			document.title = meta.title;
+			document.title = meta.creator ? " - " + meta.creator : "";
+			title.setValue(meta.title);
+			if (meta.creator) {
+				creator.setValue(meta.creator);
+				separator.dom.style.display = "inline-block";
+			}
 		});
-	}
-
-	init(meta) {
-
-		document.title = meta.title + " – " + meta.creator;
-
-		this.title.setValue(meta.title);
-		this.creator.setValue(meta.creator);
-		this.separator.dom.style.display = 'inline-block';
 	}
 }
